ae701c8dadcf7d3cbff03d1041fef2173a113a26
Yurdoy <yurdoy@gmail.com>
2025-07-02 11:35:07 +0200

    Model Book created

---
+++ src/models/book.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Book = sequelize.define(
+  "Book",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "Books",
+    timestamps: false,
+  }
+);
+
+export default Book;
